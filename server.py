c6b76bd6eab8ce1b8ec9e09e8aa083fe0950d260
richard <richard>
2001-07-20 12:33:06 +0000

    oops ;)

--- server.py
+++ server.py
@@ -3,7 +3,7 @@
 
 Stolen from CGIHTTPServer
 
-$Id: server.py,v 1.4 2001/07/19 10:43:01 anthonybaxter Exp $
+$Id: server.py,v 1.5 2001/07/20 12:33:06 richard Exp $
 
 """
 import sys
@@ -13,7 +13,7 @@ if int(sys.version[0]) < 2:
 
 __version__ = "0.1"
 
-__all__ = ["CGIHTTPRequestHandler"]
+__all__ = ["RoundupRequestHandler"]
 
 import os, urllib, StringIO, traceback, cgi, binascii
 import BaseHTTPServer
@@ -156,6 +156,9 @@ if __name__ == '__main__':
 
 #
 # $Log: server.py,v $
+# Revision 1.5  2001/07/20 12:33:06  richard
+# oops ;)
+#
 # Revision 1.4  2001/07/19 10:43:01  anthonybaxter
 # HTTP_HOST and HTTP_PORT config options.
 #
