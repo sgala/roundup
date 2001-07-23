199c55fd9b6e7d825d6ec05fdf5455762e592cf0
richard <richard>
2001-07-23 23:16:01 +0000

    Split off the interfaces (CGI, mailgw) into a separate file from the DB stuff.

--- roundup/templates/extended/__init__.py
+++ roundup/templates/extended/__init__.py
@@ -1,10 +1,14 @@
-# $Id: __init__.py,v 1.2 2001/07/23 04:33:21 anthonybaxter Exp $
+# $Id: __init__.py,v 1.3 2001/07/23 23:16:01 richard Exp $
 
 from instance_config import *
 from dbinit import *
+from interfaces import *
 
 # 
 # $Log: __init__.py,v $
+# Revision 1.3  2001/07/23 23:16:01  richard
+# Split off the interfaces (CGI, mailgw) into a separate file from the DB stuff.
+#
 # Revision 1.2  2001/07/23 04:33:21  anthonybaxter
 # split __init__.py into 2. dbinit and instance_config.
 #
