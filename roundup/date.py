a1968fa9bab6ae5896fd80f2a62bf19a1e1e8583
jhermann <jhermann>
2002-01-23 20:00:50 +0000

    %e is a UNIXism and not documented for Python

--- roundup/date.py
+++ roundup/date.py
@@ -15,7 +15,7 @@
 # BASIS, AND THERE IS NO OBLIGATION WHATSOEVER TO PROVIDE MAINTENANCE,
 # SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 # 
-# $Id: date.py,v 1.17 2002/01/16 07:02:57 richard Exp $
+# $Id: date.py,v 1.18 2002/01/23 20:00:50 jhermann Exp $
 
 __doc__ = """
 Date, time and time interval handling.
@@ -168,8 +168,10 @@ class Date:
     def pretty(self):
         ''' print up the date date using a pretty format...
         '''
-        return time.strftime('%e %B %Y', (self.year, self.month,
+        str = time.strftime('%d %B %Y', (self.year, self.month,
             self.day, self.hour, self.minute, self.second, 0, 0, 0))
+        if str[0] == '0': return ' ' + str[1:]
+        return str
 
     def set(self, spec, offset=0, date_re=re.compile(r'''
               (((?P<y>\d\d\d\d)-)?((?P<m>\d\d?)-(?P<d>\d\d?))?)? # yyyy-mm-dd
@@ -383,6 +385,9 @@ if __name__ == '__main__':
 
 #
 # $Log: date.py,v $
+# Revision 1.18  2002/01/23 20:00:50  jhermann
+# %e is a UNIXism and not documented for Python
+#
 # Revision 1.17  2002/01/16 07:02:57  richard
 #  . lots of date/interval related changes:
 #    - more relaxed date format for input
