73fb84e78945a4b98717e00dea9c1ab925008a8f
gmcm <gmcm>
2002-07-30 20:43:18 +0000

    Oops, fix the permission check!

--- roundup/cgi_client.py
+++ roundup/cgi_client.py
@@ -15,7 +15,7 @@
 # BASIS, AND THERE IS NO OBLIGATION WHATSOEVER TO PROVIDE MAINTENANCE,
 # SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 # 
-# $Id: cgi_client.py,v 1.149 2002/07/30 20:04:38 gmcm Exp $
+# $Id: cgi_client.py,v 1.150 2002/07/30 20:43:18 gmcm Exp $
 
 __doc__ = """
 WWW request handler (also used in the stand-alone server).
@@ -710,7 +710,7 @@ function help_window(helpurl, width, height) {
         if keys and not self.form.has_key('__login_name'):
             try:
                 userid = self.db.user.lookup(self.user)
-                if not self.db.security.hasPermission('Edit', userid):
+                if not self.db.security.hasPermission('Edit', userid, cn):
                     message = _('You do not have permission to edit %s' %cn)
                 else:
                     props = parsePropsFromForm(self.db, cl, self.form, self.nodeid)
@@ -1695,6 +1695,9 @@ def parsePropsFromForm(db, cl, form, nodeid=0, num_re=re.compile('^\d+$')):
 
 #
 # $Log: cgi_client.py,v $
+# Revision 1.150  2002/07/30 20:43:18  gmcm
+# Oops, fix the permission check!
+#
 # Revision 1.149  2002/07/30 20:04:38  gmcm
 # Adapt metakit backend to new security scheme.
 # Put some more permission checks in cgi_client.
